cf134e225c0699930b09536c1b9a4f722d4dfe3c
Vasan j k <114376438+Vasan-jk@users.noreply.github.com>
2025-09-07 20:18:11 +0530

    tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,45 +1,52 @@
-
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-
-
-// tb_gray_counter.v
-`timescale 1ns/1ps
+ `timescale 1ns/1ps
 
 module tb_gray_counter;
-    reg clk;
-    reg rst_n;
-    wire [7:0] gray;
 
+    // DUT signals
+    reg  clk;
+    reg  rst_n;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-     initial begin
+    // Instantiate DUT
+    tt_um_gray_counter dut (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+    initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
-    // Instantiate DUT
-    gray_counter dut (
-        .clk(clk),
-        .rst_n(rst_n),
-        .gray(gray)
-    );
-
-    // Clock generator: 10ns period = 100MHz
+    // Clock generator (10ns period = 100 MHz)
     always #5 clk = ~clk;
 
     initial begin
-        // init
-        clk = 0;
-        rst_n = 0;
+        // Init
+        clk    = 0;
+        rst_n  = 0;
+        ui_in  = 8'h00;
+        uio_in = 8'h00;
 
-        // apply reset
+        // Apply reset
         #20 rst_n = 1;
 
-        // run simulation for 20 cycles
+        // Run simulation for 20 cycles
         repeat (20) begin
             @(posedge clk);
-            $display("Time=%0t, Gray=%b", $time, gray);
+            $display("Time=%0t, Gray=%b", $time, uo_out);
         end
 
-        $stop;
+        $finish;
     end
+
 endmodule
+
